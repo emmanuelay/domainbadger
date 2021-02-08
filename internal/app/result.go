a13615d8713ff052c14ccfa1a7876cf30fa1d988
Emmanuel Ay <emmanuel@block21.com>
2021-02-08 15:44:00 +0100

    Results struct

--- internal/app/result.go
+++ internal/app/result.go
@@ -1,11 +1,16 @@
 package app
 
+import whoisparser "github.com/likexian/whois-parser-go"
+
 // Structs for reporting and accumulating progress (# available, # not available)
 
 // lookupResult encapsulates a single lookup
 type lookupResult struct {
 	Domain    string
+	TLD       string
 	Available bool
+	Error     error
+	WhoIs     whoisparser.WhoisInfo
 }
 
 // tldResult encapsulates a series of lookups for a tld
