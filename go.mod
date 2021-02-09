08b595b849c6b63281d628f7145d495750ca4ef4
Emmanuel Ay <emmanuel@block21.com>
2021-02-09 17:20:48 +0100

    Added progressbars

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/likexian/whois-parser-go v1.15.1
+	github.com/vbauerster/mpb/v6 v6.0.2
 	github.com/zonedb/zonedb v1.0.2970
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 )
--- internal/app/app.go
+++ internal/app/app.go
@@ -2,10 +2,22 @@ package app
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/emmanuelay/badger/internal/config"
 	"github.com/emmanuelay/badger/pkg/combinations"
+
+	"github.com/vbauerster/mpb/v6"
+	"github.com/vbauerster/mpb/v6/decor"
 )
+
+type report struct {
+	TLD          string
+	TotalCount   int
+	CurrentCount int
+	Results      []lookupResult
+	Bar          *mpb.Bar
+}
 
 // Run ...
 func Run(cfg config.Configuration) {
@@ -14,43 +26,70 @@ func Run(cfg config.Configuration) {
 	doneChannel := make(chan string, 1)
 	intDelay := int(cfg.Delay)
 	alphaSet := []rune(cfg.Characters)
-	tldCount := 0
+	tldCount := len(cfg.TLD)
 
-	// For each search pattern
-	for _, searchPattern := range cfg.SearchPatterns {
+	reports := map[string]report{}
 
-		// Run generation of unique combinations
-		uniqueCombinations := combinations.GenerateNames(alphaSet, searchPattern, "_")
+	fmt.Println("Performing lookups")
 
-		// For each TLD
-		for _, tld := range cfg.TLD {
+	var wg sync.WaitGroup
+	p := mpb.New(mpb.WithWaitGroup(&wg), mpb.WithWidth(40))
 
+	// For each TLD
+	for _, tld := range cfg.TLD {
+
+		rep := report{
+			TLD:          tld,
+			CurrentCount: 0,
+			Results:      []lookupResult{},
+		}
+
+		// For each search pattern
+		for _, searchPattern := range cfg.SearchPatterns {
+
+			// Run generation of unique combinations
+			uniqueCombinations := combinations.GenerateNames(alphaSet, searchPattern, "_")
+
+			rep.TotalCount = rep.TotalCount + len(uniqueCombinations)
+
+			wg.Add(1)
 			// Run lookup for the TLD and the unique combinations generated from the search pattern
 			go lookupDomainsForTLD(searchPattern, uniqueCombinations, tld, intDelay, progressChannel, doneChannel)
 		}
 
-		tldCount = tldCount + len(cfg.TLD)
+		name := fmt.Sprintf("%v:", tld)
+		bar := p.AddBar(int64(rep.TotalCount),
+			mpb.PrependDecorators(
+				decor.Name(name, decor.WC{W: 6, C: decor.DidentRight}),
+			),
+			mpb.AppendDecorators(
+				decor.Percentage(decor.WCSyncSpace),
+			),
+			mpb.AppendDecorators(
+				decor.OnComplete(
+					decor.AverageETA(decor.ET_STYLE_GO), " ",
+				),
+			),
+		)
+		rep.Bar = bar
+		reports[tld] = rep
 	}
 
-	results := map[string][]lookupResult{}
-
-	// TODO(ea): Create Multi Progress Bar
 	for {
 		select {
-		case val := <-doneChannel:
+		case _ = <-doneChannel:
 			{
-				// TODO(ea): Update progress bar
-				fmt.Println("Scan completed for TLD:", val)
 				tldCount--
+				wg.Done()
 			}
 		case result := <-progressChannel:
 			{
-				// TODO(ea): Update progress bar
-				if result.Available {
-					fmt.Println("-", result.Domain, "is available")
-				}
+				rep := reports[result.TLD]
+				rep.CurrentCount++
+				rep.Results = append(rep.Results, result)
+				reports[result.TLD] = rep
 
-				results[result.TLD] = append(results[result.TLD], result)
+				rep.Bar.Increment()
 			}
 		}
 
@@ -59,10 +98,11 @@ func Run(cfg config.Configuration) {
 		}
 	}
 
-	// TODO(ea): compile and display results nicely
+	p.Wait()
 
-	for idx, tld := range results {
-		fmt.Println("-", len(tld), idx)
+	// TODO(ea): compile and display results nicely
+	for idx, rep := range reports {
+		fmt.Println("-", rep.TLD, len(rep.Results), idx, rep.CurrentCount, rep.TotalCount)
 	}
 
 }
