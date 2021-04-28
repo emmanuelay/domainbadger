73ad208e682b03bba517242edd9baabaf252bff0
Emmanuel Ay <emmanuel@block21.com>
2021-04-28 23:41:54 +0200

    Renamed structs, removed unused

--- internal/app/result.go
+++ internal/app/result.go
@@ -4,17 +4,21 @@ import whoisparser "github.com/likexian/whois-parser-go"
 
 // Structs for reporting and accumulating progress (# available, # not available)
 
-// lookupResult encapsulates a single lookup
-type lookupResult struct {
+// TLDResults encapsulates all lookups performed within a TLD
+type TLDResults struct {
+	TLD         string
+	Results     []DomainLookupResult
+	TotalCount  int64
+	Available   int64
+	Unavailable int64
+	ErrorCount  int64
+}
+
+// DomainLookupResult encapsulates a single domain lookup
+type DomainLookupResult struct {
 	Domain    string
 	TLD       string
 	Available bool
 	Error     error
 	WhoIs     whoisparser.WhoisInfo
-}
-
-// tldResult encapsulates a series of lookups for a tld
-type tldResult struct {
-	TLD     string
-	Lookups []lookupResult
 }
